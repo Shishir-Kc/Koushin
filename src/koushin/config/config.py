5df7a9e96cfe80639f2c2021e2851ccb958bce76
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-24 16:26:02 +0545

    chor : added function to read

--- src/koushin/config/config.py
+++ src/koushin/config/config.py
@@ -6,9 +6,10 @@ Note:   working on to create reading config.koushin
 
 """
 from pathlib import Path
+import configparser
 
 
-def create_file(github:str,path = Path.cwd()):
+def create_config(github:str,path = Path.cwd()):
     """ 
     This function will create config.koushin which will contain the following
     
@@ -32,6 +33,28 @@ version = 0.0.1
     with open(f"{path}/config.koushin","w")as file:
         file.write(TEMPLATE)        
 
+def read_config(path = Path.cwd()):
+    """
+    
+    This function will read config.koushin and will return the following:
+    
+    Returns:
+    
+    github : github repo for the project 
 
-create_file(github="https://github.com/Shishir-Kc/Koushin")
+    version-manager : will return config.kosun raw file 
 
+    version : current version 
+
+    """
+
+    config= configparser.ConfigParser()
+    config.read(f"{path}/config.koushin")
+    return {
+        "github":config["github"]["repo"],
+        "version-manager":config["version"]["version-manager"],
+        "version":config["version"]["version"]
+    }
+
+# create_config(github="https://github.com/Shishir-Kc/Koushin")
+read_config()

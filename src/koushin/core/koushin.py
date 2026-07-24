c1cb454246056dfacac268b1f0bc5d28657e6c4c
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-24 16:26:51 +0545

    chor: implimenting shell for the logic

---
+++ src/koushin/core/koushin.py
@@ -0,0 +1,14 @@
+"""
+This file will have logic for the following :
+check current version , and update 
+
+"""
+
+class Updater:
+    def __init__(self) -> None:
+        pass
+    def check_verison(self):
+        pass
+    def update(self):
+        pass
+

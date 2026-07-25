9ab27dd085e9e7b6e493e9cd7ddcc027a93a77ae
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-25 08:51:21 +0545

    chor: implementing methods to check current project version and implemenmting method to update the project

--- src/koushin/core/koushin.py
+++ src/koushin/core/koushin.py
@@ -3,12 +3,35 @@ This file will have logic for the following :
 check current version , and update 
 
 """
+from koushin.config.config import read_config
 
 class Updater:
     def __init__(self) -> None:
         pass
+    
     def check_verison(self):
-        pass
-    def update(self):
-        pass
+        """ 
+            This method will check the version of current project 
 
+            Returns:
+
+            {
+            'current_version':'0.0.1'
+            }
+
+        """
+        project_metadata= read_config()
+        return {
+            "current_version":project_metadata.get("version","")
+        }
+
+    def update(self):
+        """
+            This method will update the projetc to the latest available version 
+        """
+        local_metadata = read_config()
+
+        return local_metadata 
+update = Updater()
+print(update.check_verison())
+print(update.update())

2bfa1dd690e833ac26749a201e2fea1d6df7b303
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-25 18:13:57 +0545

    implemnting updater

--- src/koushin/core/koushin.py
+++ src/koushin/core/koushin.py
@@ -3,7 +3,16 @@ This file will have logic for the following :
 check current version , and update 
 
 """
-from koushin.config.config import read_config
+from re import sub
+from src.koushin.config.config import (
+    read_config, get_config,
+    conversion, generate_clean_path 
+)
+import subprocess
+import os
+from pathlib import Path
+import shutil
+
 
 class Updater:
     def __init__(self) -> None:
@@ -13,7 +22,7 @@ class Updater:
         """ 
             This method will check the version of current project 
 
-            Returns:
+            Returns: (eg)
 
             {
             'current_version':'0.0.1'
@@ -30,8 +39,21 @@ class Updater:
             This method will update the projetc to the latest available version 
         """
         local_metadata = read_config()
+        cloud_metadata = get_config()
+        github_repo = cloud_metadata.get("github","") #type:ignore
+        installation_path = cloud_metadata.get("install_path") #type:ignore
+        local_version = local_metadata.get("version","")
+        cloud_version = cloud_metadata.get("version","") #type:ignore 
+        if conversion(cloud_version) > conversion(local_version):
+            local_installation_path = local_metadata.get("install_path","")
+            project_name = local_metadata.get("project_name","")
+            installation_path = generate_clean_path(path=local_installation_path,project_name=project_name)
+            print(f"new update will be installed for {project_name} on {installation_path} form repo {github_repo}")
+            for entry in os.scandir(local_installation_path):
+                if entry.is_dir(follow_symlinks=False):
+                    shutil.rmtree(entry.path)
+            os.chdir(os.path.expanduser("~"))
+            subprocess.run(f"git clone {github_repo} {installation_path}",shell=True)
+            os.chdir(local_installation_path)
+            subprocess.run("uv sync",shell=True)
 
-        return local_metadata 
-update = Updater()
-print(update.check_verison())
-print(update.update())
